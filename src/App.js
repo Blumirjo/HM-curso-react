34546ecaf8e79955c5fa31e2406950d6576dbd19
Sebastian Gatica <expandiendoartemusical@gmail.com>
2022-03-08 14:26:30 -0300

    momento

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Input extends Component {
+ 
+  render() {
+    return(
+      <input 
+        value={this.props.value}
+        onChange={this.props.onChange}
+      />
+    )
+  }
 }
+
+class App extends Component {
+  state = {
+    nombre:'',
+    apellido: '',
+  }
+
+  updateValues = (prop,value)=> {
+    this.setState({[prop]: value})
+  }
+  render(){
+    return(
+      <p>
+        Nombre completo:{`${this.state.nombre} ${this.state.apellido}`}
+        <Input
+        value={this.state.nombre}
+        onChange={e => this.updateValues("nombre", e.target.value)}
+        />
+        <Input 
+        value={this.state.apellido}
+        onChange={e => this.updateValues("apellido", e.target.value)}
+        />
+      </p>
+    )
+  }
+}
+
+
+
+//----------------------------
+
+// class Input extends Component {
+//   state = {valor: ""}
+
+//   handleChange = (value) => {
+//     this.setState({ valor: value })
+//   }
+//   render() {
+//     return(
+//       <input 
+//         value={this.state.value}
+//         onChange={e => this.handleChange(e.target.value)}
+//       />
+//     )
+//   }
+// }
+
+// class App extends Component {
+//   render(){
+//     return(
+//       <p>
+//         Nombre completo:
+//         <Input />
+//         <Input />
+//       </p>
+//     )
+//   }
+// }
+
+
+//---------------------------------------------------------
+
+//  class Button extends Component {
+//    state = {};
+   
+//    constructor(props) {
+//      super(props)
+//      console.log('constructor', props)
+//    }
+
+//    componentDidMount(){
+//      console.log("componentDidMount")
+//    }
+
+//    componentDidUpdate(prevProps, PrevState){
+//      console.log('componentDidUpdate', prevProps,PrevState)
+//    }
+
+//    componentWillUnmount(){
+//      console.log('desmontando component', this.props, this.state)
+//    }
+
+//    render(){
+//      console.log('Ejecutando metodo render de button');
+//      return(
+//        <button onClick={()=> this.setState({prop: 1})}>
+//          Enviar
+//        </button>
+//      )
+//    }
+//  }
+ 
+// class App extends Component {
+//     state = {
+//       valor: 3
+//     };
+
+  
+
+//   render() {
+//      console.log(this.state);
+//     return(
+//       <div>
+//       <p>
+//         Hola mundo
+//       </p>
+//       {this.state.valor === 3 ? <Button chanchito='feliz' /> : null}
+//       <button className={`${this.state.valor}`} onClick={() => this.setState({valor: 2})}>
+//         Enviaren App
+//       </button>
+//       </div>
+     
+//     )
+//   }
+// }
 
 export default App;
